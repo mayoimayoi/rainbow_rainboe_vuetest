fa53af504b49ed5f0d2bd0ac103c2cf285763968
mayoimayoi <tky529@gmail.com>
2023-02-23 22:10:40 +0900

    TopImg error change

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,7 +3,7 @@ import TopImg from "./../assets/TopImg.png";
 </script>
 <template>
   <header>
-    <img :src="TopImg" alt="トップ画像" class="topImg pt-3" />
+    <img :src="TopImg" alt="トップ画像" class="topImg pt-2" />
   </header>
 </template>
 
